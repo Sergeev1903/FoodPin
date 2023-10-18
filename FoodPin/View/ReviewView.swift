a894dc604c3e4a61bceaa0777bfd67c27f0b345e
Artem Sergeev <sergeev190391@gmail.com>
2023-10-18 15:36:42 +0300

    model with ObservableObject

---
+++ FoodPin/View/ReviewView.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewView.swift
+//  FoodPin
+//
+//  Created by Артем Сергеев on 10.10.2023.
+//
+
+import SwiftUI
+
+struct ReviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewView()
+}
