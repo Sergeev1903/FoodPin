c7889627286245d9f971c755ce84c7a3654b3dad
Artem Sergeev <sergeev190391@gmail.com>
2023-10-21 16:59:54 +0300

    fix bug in detail screen & add tutorial walkthrough

---
+++ FoodPin/View/Tutorial/TutorialPage.swift
@@ -0,0 +1,46 @@
+//
+//  TutorialView.swift
+//  FoodPin
+//
+//  Created by Артем Сергеев on 21.10.2023.
+//
+
+import SwiftUI
+
+
+struct TutorialPage: View {
+	
+	let image: String
+	let heading: String
+	let subHeading: String
+	
+	var body: some View {
+		VStack(spacing: 70) {
+			Image(image)
+				.resizable()
+				.scaledToFit()
+			
+			VStack(spacing: 10) {
+				Text(heading)
+					.font(.headline)
+				
+				Text(subHeading)
+					.font(.body)
+					.foregroundColor(.gray)
+					.multilineTextAlignment(.center)
+			}
+			.padding(.horizontal, 40)
+			
+			Spacer()
+		}
+		.padding(.top)
+	}
+}
+
+
+
+#Preview {
+	TutorialPage(image: "onboarding-1", heading: "CREATE YOUR OWN FOOD GUIDE", subHeading: "Pin your favorite restaurants and create your own food guide")
+		.previewLayout(.sizeThatFits)
+		.previewDisplayName("TutorialPage")
+}
---
+++ FoodPin/View/Tutorial/TutorialView.swift
@@ -0,0 +1,18 @@
+//
+//  TutorialView.swift
+//  FoodPin
+//
+//  Created by Артем Сергеев on 21.10.2023.
+//
+
+import SwiftUI
+
+struct TutorialView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TutorialView()
+}
